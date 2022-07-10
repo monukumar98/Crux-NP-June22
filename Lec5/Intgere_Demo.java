5063a236ee14339642d253a9ef55abd88ac01904
monukumar98 <monu012as@gmail.com>
2022-07-10 23:53:07 +0530

    Data Type

---
+++ Lec5/Intgere_Demo.java
@@ -0,0 +1,16 @@
+package Lec5;
+
+public class Intgere_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 45;
+		byte b = -127;
+		short s = 89;
+		long l = 78;
+		boolean b1=true;
+
+	}
+
+}
---
+++ Lec5/Inverse_of_number.java
@@ -0,0 +1,10 @@
+package Lec5;
+
+public class Inverse_of_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
