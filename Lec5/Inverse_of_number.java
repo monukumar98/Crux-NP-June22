5848a98e36f39e40a264cbf0ea7fba5210fd0867
monukumar98 <monu012as@gmail.com>
2022-07-16 15:46:15 +0530

    Data Type-2

--- Lec5/Inverse_of_number.java
+++ Lec5/Inverse_of_number.java
@@ -1,10 +1,23 @@
 package Lec5;
 
+import java.util.Scanner;
+
 public class Inverse_of_number {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		int place = 1;
+		while (n > 0) {
+			int rem = n % 10;
+			sum = (int) (sum + place * Math.pow(10, rem-1));
+			n = n / 10;
+			place++;
 
+		}
+		System.out.println(sum);
 	}
 
 }
