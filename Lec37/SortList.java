595206ed436656d3c85fa71085ecf49f857c0b7d
monukumar98 <monu012as@gmail.com>
2022-11-27 19:19:07 +0530

    LinkedList-Qps

--- Lec37/SortList.java
+++ Lec37/SortList.java
@@ -20,8 +20,56 @@ public class SortList {
 
 	class Solution {
 		public ListNode sortList(ListNode head) {
-			
-			
+			if (head == null || head.next == null) {
+				return head;
+			}
+
+			ListNode mid = middleNode(head);
+			ListNode midnext = mid.next;
+			mid.next = null;
+			ListNode A = sortList(head);
+			ListNode B = sortList(midnext);
+			return mergeTwoLists(A, B);
+
+		}
+
+		public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+			ListNode A = list1;
+			ListNode B = list2;
+			ListNode Dummy = new ListNode();
+			ListNode temp = Dummy;
+			while (A != null && B != null) {
+				if (A.val < B.val) {
+					Dummy.next = A;
+					Dummy = Dummy.next;
+					A = A.next;
+				} else {
+					Dummy.next = B;
+					Dummy = Dummy.next;
+					B = B.next;
+				}
+
+			}
+
+			if (A != null) {
+				Dummy.next = A;
+			}
+			if (B != null) {
+				Dummy.next = B;
+			}
+			return temp.next;
+
+		}
+
+		public ListNode middleNode(ListNode head) {
+			ListNode slow = head;
+			ListNode fast = head;
+			while (fast.next != null && fast.next.next != null) {
+				slow = slow.next;
+				fast = fast.next.next;
+			}
+			return slow;
 
 		}
 	}
